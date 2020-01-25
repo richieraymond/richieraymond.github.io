cba03b8c0b46f275beb4a8847ee99be8513b0842
jeanjoe <manzede@gmail.com>
2020-01-25 13:35:12 +0300

    Pushing

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ var submitButton= document.getElementById('submitButton');
 var table= document.getElementById('blogTable');
 var viewButton = document.getElementById('viewButton');
 var confirmDelete = document.getElementById('confirmAction');
+var addPost = document.getElementById('addPost');
+var editButton = document.getElementById('addPost');
+
+var clearForm = document.getElementById('clearForm');
+localStorage.clear();
 var declineAction =document.getElementById('declineAction');
 var postArray=[]=[];
 var rowIndex;
 var title;
 var body;
-
 postTitle.addEventListener('change',(e)=>{
 	title = e.target.value;
 });
@@ -17,27 +21,56 @@ postBody.addEventListener('change',(e)=>{
 	body = e.target.value;
 });
 submitButton.addEventListener('click',(e)=>{
-	let postObj = {blogTitle:title,blogBody:body};
-	postArray.push(postObj);
-	let blogs = JSON.parse(localStorage.getItem('blogPosts'));
-	blogs.push(postArray);
-	localStorage.setItem('blogPosts',JSON.stringify(blogs))
+	postObj={blogTitle:title,blogBody:body};
+	if(rowIndex){
+		let itemIndex = parseInt(rowIndex)-1;
+		postArray.splice(itemIndex,1,postObj)
+	}else{
+		postArray.push(postObj);
+	}
+	localStorage.setItem('blogPosts',JSON.stringify(postArray))
 	postTitle.value='';
 	postBody.value='';
+	showAlert('alert','Post Added Successfully','alertContent');
+	document.getElementById('blogform').style.display="none"
 	console.log(localStorage.getItem('blogPosts'))
 });
 
-confirmDelete.addEventListener('click',(e)=>{
-	let blogs = JSON.parse(localStorage.getItem('blogPosts'));
-	let index = parseInt(rowIndex)-1;
-	blogs.slice(index,1)
-	localStorage.setItem('blogPosts',JSON.stringify(blogs))
-	document.getElementById('alertContent').innerHTML="Post Deleted Successfully";
+function showAlert(alertId,alertMessage,alertMessageId){
+	document.getElementById(alertMessageId).innerHTML=alertMessage;
+	document.getElementById(alertId).style.display="block";
 	setTimeout(val=>{
-		document.getElementById('alert').style.display="none";
-		window.location = "index.html";
+		closeNotification(alertId);
 	},1000)
+
+}
+function closeNotification(alertId){
+	document.getElementById(alertId).style.display="none";
+}
+
+confirmDelete.addEventListener('click',(e)=>{
+	let itemIndex = parseInt(rowIndex)-1;
+	postArray.splice(itemIndex,1)
+	localStorage.setItem('blogPosts',JSON.stringify(postArray))
+	showAlert('alert','Record Deleted Successfully!','alertContent')
+	document.getElementById('modal-center').style.display="none";
+	viewButton.click();
 });
+
+function edit(){
+	let itemIndex = parseInt(rowIndex)-1;
+	console.log(postArray)
+	if(postArray.length>0){
+		let postToEdit = postArray[itemIndex];
+		postTitle.value=postToEdit.blogTitle;
+		postBody.value=postToEdit.blogBody;
+		document.getElementById('blogTable').style.display="none";
+		document.getElementById('blogform').style.display="inline";
+	}
+
+
+}
+	
 
 declineAction.addEventListener('click',(e)=>{
 	document.getElementById('modal-center').style.display="none"
@@ -50,22 +83,38 @@ function  getId(element) {
 
 viewButton.addEventListener('click',(e)=>{
 	let blogs = JSON.parse(localStorage.getItem('blogPosts'));
-	document.getElementById('totalPosts').innerHTML=blogs.length;
+	if(blogs){
+		document.getElementById('totalPosts').innerHTML=blogs.length;
+		console.log(blogs)
+		var tableBody='';
+		let i = 0;
+		blogs.forEach(val=>{
+			i+=1;
+			tableBody=tableBody+'<tr>'+
+			'<td>'+i+'</td>'+
+			'<td>'+val.blogTitle+'</td>'+
+			'<td>'+val.blogBody+'</td>'+
+			'<td>'+ '<a class="uk-icon-link" uk-icon="trash" onclick="getId(this)" href="#modal-center" uk-toggle></a>'+   '<a href="#" class="uk-icon-link uk-margin-small-right" uk-icon="file-edit" onclick="getId(this);edit()"></a>'+'</td>'+
+		'</tr>';
+		})
+		console.log(tableBody)
+		document.getElementById('tableBody').innerHTML=tableBody;
+		document.getElementById('blogform').style.display="none";
+		document.getElementById('blogTable').style.display="inline";
+	}else{
+		document.getElementById('totalPosts').innerHTML=0;
+	}
+})
+addPost.addEventListener('click',(e)=>{
+	document.getElementById('blogform').style.display="inline";
+	document.getElementById('blogTable').style.display="none";
 
-	var tableBody='';
-	let i = 0;
-	blogs.forEach(val=>{
-		i+=1;
-		tableBody=tableBody+'<tr>'+
-		'<td>'+i+'</td>'+
-		'<td>'+val.blogTitle+'</td>'+
-		'<td>'+val.blogBody+'</td>'+
-		'<td>'+ '<a class="uk-icon-link" uk-icon="trash" onclick="getId(this)" href="#modal-center" uk-toggle></a>'+   '<a href="#" class="uk-icon-link uk-margin-small-right" uk-icon="file-edit"></a>'+'</td>'+
-	'</tr>';
-	})
-	console.log(tableBody)
-	document.getElementById('tableBody').innerHTML=tableBody;
 })
 
+clearForm.addEventListener('click',(e)=>{
+	postTitle.value="";
+	postBody.value="";
+
+})
 
 
